996283783e6a71e98db2379952abc7dcb7c7bf4e
dbond <debon1426@gmail.com>
2020-02-09 14:45:33 -0500

    finish test cases for Scientific class

--- src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
@@ -2,19 +2,19 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Basic {
 
-    public int add(int x, int y) {
+    public double add(double x, double y) {
         return x + y;
     }
 
-    public int multiply(int x, int y) {
+    public double multiply(double x, double y) {
         return x * y;
     }
 
-    public int divide(int x, int y) {
+    public double divide(double x, double y) {
         return x / y;
     }
 
-    public int subtract(int x, int y) {
+    public double subtract(double x, double y) {
         return x - y;
     }
 }
