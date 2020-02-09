204bb3f5626b3f523395e5d22ddeb53d895bdcef
dbond <debon1426@gmail.com>
2020-02-09 00:56:00 -0500

    finish test cases for Scientific class

--- src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
@@ -2,16 +2,19 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Basic {
 
-    public int add (int x , int y){
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
-    public int multiply ( int x, int y){
-         return x*y;
+
+    public int multiply(int x, int y) {
+        return x * y;
     }
-    public int divide ( int x, int y){
-        return x/y;
+
+    public int divide(int x, int y) {
+        return x / y;
     }
-    public int subtract (int x, int y){
-        return x-y;
+
+    public int subtract(int x, int y) {
+        return x - y;
     }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -1,67 +1,65 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Scientific {
-    public int square(double x ){
+    public int square(double x) {
 
-        return  (int) Math.pow(x,2);
+        return (int) Math.pow(x, 2);
     }
 
-    public int squareRoot( double x){
+    public int squareRoot(double x) {
         return (int) Math.sqrt(x);
     }
 
-    public double inverse ( int x){
+    public double inverse(int x) {
         Basic basic = new Basic();
-        return basic.divide(1,x);
+        return basic.divide(1, x);
     }
-public double switchSign (double  x){
-    return x*(-1);
 
-}
-public double sinX( double x ){
-        double radians=Math.toRadians(x);
+    public double switchSign(double x) {
+        return x * (-1);
 
-        return Math.sin(radians);
-}
+    }
 
-    public double sin( double y ){
-        double radians=Math.toRadians(y);
+
+    public double sin(double y) {
+        double radians = Math.toRadians(y);
 
         return Math.sin(radians);
     }
 
 
-    public double cos( double x ){
-        double radians=Math.toRadians(x);
+    public double cos(double x) {
+        double radians = Math.toRadians(x);
 
         return Math.cos(radians);
     }
 
-    public double tangent( double x){
-        double radians=Math.toRadians(x);
+    public double tangent(double x) {
+        double radians = Math.toRadians(x);
 
         return Math.tan(radians);
 
 
     }
-    public double  inverseSine ( double x){
-        return Math.asin(sin(x));
+
+    public double inverseSine(double x) {
+        return Math.asin(x);
     }
-    public double  inverseCosine ( double x){
+
+    public double inverseCosine(double x) {
         return Math.acos(sin(x));
     }
-    public double  inverseTangent ( double x){
-        return Math.atan(sin(x));
-    }
-    public double factorial( double x){
-        double num=0;
-        double damn=0;
-     for ( double i=x; i>0; i--){
-         double previousNum=i-1;
-         num=previousNum*i;
-         damn+=num;
 
-     } return num;
+    public double inverseTangent(double x) {
+        return Math.atan(x);
     }
 
+    public double factorial(double x) {
+        double num = 1;
+
+        for (double i = x; i > 0; i--) {
+            num = num * i;
+        }
+        return num;
+    }
 }
