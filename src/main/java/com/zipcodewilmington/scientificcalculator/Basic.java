3626d307a8d006d2c17d10f32652da662217a51d
dbond <debon1426@gmail.com>
2020-02-08 22:21:30 -0500

    Basic and scientific

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Basic.java
@@ -0,0 +1,17 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Basic {
+
+    public int add (int x , int y){
+        return x+y;
+    }
+    public int multiply ( int x, int y){
+         return x*y;
+    }
+    public int divide ( int x, int y){
+        return x/y;
+    }
+    public int subtract (int x, int y){
+        return x-y;
+    }
+}
---
+++ src/main/test/java/com/zipcodewilmington/scientificcalculator/ScientificTest.java
@@ -0,0 +1,35 @@
+package com.zipcodewilmington.scientificcalculator;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScientificTest {
+
+    @Test
+    public void squareTest() {
+        Scientific scientific = new Scientific();
+        int expected = 4;
+        int actual= scientific.square(2);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void sinTest() {
+        Scientific scientific = new Scientific();
+        double expected = 0.7071067811865475;
+        double actual= scientific.sinX(45.0);
+        Assert.assertEquals(expected,actual,0);
+        // delta is the percision lost
+    }
+      @Test
+    public void factorialTest(){
+          Scientific scientific = new Scientific();
+          double expected = 24;
+          double actual= scientific.factorial(4);
+          Assert.assertEquals(expected, actual,0);
+
+      }
+
+
+
+}
