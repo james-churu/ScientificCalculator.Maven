271cebcf4e18831e70989038bd2f3246eeb2236a
jchuru <jameschuru123@gmail.com>
2020-02-09 20:35:06 -0500

    calculator fully runs

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -16,36 +16,51 @@ public class Calculator {
         currentVal = start;
         savedVal = saved;
     }
+
     //***********************Taking a number method;****************************
-    public static double numberInput() {
+    public static double numberInput(double savedNumber,double currentValue) {
         int x = 1;
-        double entered = 0;
+        Double entered = null;
         System.out.print("Enter Next Number    : ");
 
         do {
             try {
+                String inputo = "";
                 Scanner attempt1 = new Scanner(System.in);
-                entered = attempt1.nextDouble();
-                x = 0;
-                if (entered == 0) {
-                    System.out.print("Sorry 0 not valid    : ");
-                    x = 1;
+                inputo  = attempt1.nextLine();
+
+                if (inputo.equals("MRC")){
+                    entered = savedNumber;
+                    System.out.println("Using Saved #        : " + savedNumber);
+                    x = 4;
+                }else {
+                    entered = Double.parseDouble(inputo);
+                    x = 2;
                 }
-            } catch (InputMismatchException e) {
+
+                if(entered == 0) {
+                System.out.print("Sorry 0 not valid    : ");
+                x = 1;
+                }
+            }
+            catch (NumberFormatException e){
                 System.out.print("Error : Enter a #    : ");
             }
 
         } while (x == 1);
 
-        return entered;
+        if (x == 4){
+            return savedNumber;
+        }else {
+            return entered;
+        }
     }
     //***************************Taking an operator****************************
     public String operatorInput() {
 
         String[] basic = {"+", "-", "/", "*"};
         String[] sci = {"square()", "squareRoot()", "inverse()", "switchSign()", "sine()", "cosine()", "tangent()", "inverseSine()", "inverseCosine()", "inverseTangent()", "factorial()"};
-        //String[]
-        String[] quick = {"square()", "squareRoot()", "sine()", "cosine()", "tangent()", "inverseSine()", "inverseCosine()", "inverseTangent()","clear","",};
+        String[] quick = {"square()", "squareRoot()", "sine()", "cosine()", "tangent()", "inverseSine()", "inverseCosine()", "inverseTangent()","M+","MC","clear","exit"};
         String entered = "";
 
         boolean exitDoLoop = false;
@@ -115,7 +130,7 @@ public class Calculator {
             } else if (whichCalc.equals("B") || whichCalc.equals("b")) {
                 System.out.println("\n" + "Opening Basic Calc");
                 //Call Basic Calc Class
-                loopExit = 2;
+                loopExit = 1;
             } else {
                 System.out.print("Sorry invalid input : Try again : ");
 
@@ -137,11 +152,11 @@ public class Calculator {
         if (loopExit == 1) {
 
             System.out.print("Hello Please provide your starting Num : ");
-            starterNum = numberInput();
+            starterNum = numberInput(user.savedVal,user.currentVal);
             user.currentVal = starterNum;
 
             System.out.print("Hello Please provide a number to save  : ");
-            savedNum = numberInput();
+            savedNum = numberInput(user.savedVal,user.currentVal);
             user.savedVal = savedNum;
 
             int keepGoing = 1;
@@ -150,27 +165,29 @@ public class Calculator {
                 System.out.print("Enter Operator       : ");
                 String userOperator = user.operatorInput();
 
+
+
                 if(userOperator.equals("+")){
 
-                    double userNextNum = user.numberInput();
+                    double userNextNum = user.numberInput(user.savedVal,user.currentVal);
                     double newNum = basic.add(user.currentVal,userNextNum);
                     user.currentVal = newNum;
 
                 }else if(userOperator.equals("-")){
 
-                    double userNextNum = user.numberInput();
+                    double userNextNum = user.numberInput(user.savedVal,user.currentVal);
                     double newNum = basic.subtract(user.currentVal,userNextNum);
                     user.currentVal = newNum;
 
                 }else if(userOperator.equals("*")){
 
-                    double userNextNum = user.numberInput();
+                    double userNextNum = user.numberInput(user.savedVal,user.currentVal);
                     double newNum = basic.multiply(user.currentVal,userNextNum);
                     user.currentVal = newNum;
 
                 }else if(userOperator.equals("/")){
 
-                    double userNextNum = user.numberInput();
+                    double userNextNum = user.numberInput(user.savedVal,user.currentVal);
                     double newNum = basic.divide(user.currentVal,userNextNum);
                     user.currentVal = newNum;
                     //**********************************************
@@ -228,16 +245,30 @@ public class Calculator {
 
                     double newNum = scientific.factorial(user.currentVal);
                     user.currentVal = newNum;
-                }else {
+
+                }else if(userOperator.equals("M+")) {
+
+                    user.savedVal += user.currentVal;
+                    System.out.println("Memory value Now   = : " + user.savedVal);
+
+                }else if(userOperator.equals("MC")) {
+
+                    user.savedVal = 0;
+                    System.out.println("Memory value Now   = : " + user.savedVal);
+
+                }else if(userOperator.equals("clear")) {
+                    user.currentVal = 0;
+                    user.savedVal = 0;
+                    System.out.println("Current Val          : " + user.currentVal);
+                    double userNextNum = user.numberInput(user.savedVal,user.currentVal);
+                    user.currentVal = userNextNum;
+                }else if(userOperator.equals("exit")){
+                    keepGoing = 0;
                 }
-
-                keepGoing = 0;
-                
-
             }
         }
 
-        System.out.println("Your new num = " + user.currentVal);
+        System.out.println("\n" + "\n" +"  Calculator Application Closed  " + user.currentVal);
 
     }
 
@@ -246,3 +277,41 @@ public class Calculator {
 }
 
 
+/*
+do {
+            try {
+
+                Scanner attempt1 = new Scanner(System.in);
+                entered = attempt1.nextDouble();
+                x = 0;
+                if (entered == 0) {
+                    System.out.print("Sorry 0 not valid    : ");
+                    x = 1;
+                }
+                if(entered.toString().equals("MRC")) {
+                    entered = savedNumber;
+                    x = 4;
+                    break;
+                }
+            }
+            catch (InputMismatchException e) {
+
+                if(x == 4){
+                    System.out.println("Adding Saved #       : " + savedNumber);
+                    break;
+                }else {
+                    System.out.print("Error : Enter a #    : ");
+                }
+            }
+
+        } while (x == 1);
+
+        if (x == 4){
+            return savedNumber;
+        }else {
+            return entered;
+        }
+    }
+
+
+ */
